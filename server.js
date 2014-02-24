39563945208f1898e8fb5795f9e9c88d81241a74
Pavel Shum <pavel.shum@optus.com.au>
2014-02-24 15:11:57 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+/**
+ * Simple HTTP server, expressjs
+ */
+
+var express = require('express');
+var app = express();
+var public_folder = '';
+
+// Default folder is the public sub-folder.
+app.use(express.static(__dirname + '/' + public_folder);
+
+app.listen(3333);
+
+console.log('Server running, to view: http://localhost:3333');
